c56837b304ba7b078cbc723eba66bff82f7b568f
Jessy <japarker0421@gmail.com>
2022-11-08 12:15:13 -0700

    Got functions in routine, not working yet

--- db/routines.js
+++ db/routines.js
@@ -1,18 +1,64 @@
 const client = require('./client');
 
 async function getRoutineById(id){
+  try {
+  const{ rows: [routine]}= await client.query(`
+    SELECT *
+    FROM routines
+    WHERE id=$1;
+  `, [id]);
+  if(!routine){
+    throw{
+      name:"RoutineNotFoundError",
+      message:"Could not find a routine with that id"
+    };
+  }
+  return routine;
+} catch (error) {
+  console.error(error);
 }
+}
+
 
 async function getRoutinesWithoutActivities(){
 }
 
 async function getAllRoutines() {
+  try{
+    const {rows: routineIds} = await client.query(`
+    SELECT id
+    FROM routines;
+    `);
+    console.log(routineIds, "this is routineIds")
+    const routines= await Promise.all(
+    routineIds.map((routine)=> getRoutineById(routine.id))
+    );
+    return routines;
+  }catch(error){
+    console.error(error);
+  }
+
 }
 
 async function getAllRoutinesByUser({username}) {
+  try {const {rows: routineIds}= await client.query(`
+  SELECT id
+  FROM routines
+  WHERE id=${username};
+  `);
+  const routines= await getRoutineById(routineIds.id)
+  // const routines = await Promise.all(
+  //   routineIds.map((routine)=> getRoutineById(routine.id))
+  // );
+    return routines;
+  } catch (error) {
+    console.error(error)
+  }
 }
 
 async function getPublicRoutinesByUser({username}) {
+
+
 }
 
 async function getAllPublicRoutines() {
@@ -22,6 +68,18 @@ async function getPublicRoutinesByActivity({id}) {
 }
 
 async function createRoutine({creatorId, isPublic, name, goal}) {
+  try {
+    const{rows:[routine]}=await client.query(`
+    INSERT INTO routines("creatorId", "isPublic", name, goal)
+    VALUES($1, $2, $3, $4)
+    RETURNING *;
+    `, [creatorId, isPublic, name, goal]);
+    return routine
+
+  }catch (error) {
+    console.error(error)
+
+  }
 }
 
 async function updateRoutine({id, ...fields}) {
